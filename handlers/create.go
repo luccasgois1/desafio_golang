afc764411f933456d17af5f993ca3767752c7d18
luccasgois <luccasgois1@gmail.com>
2022-08-18 23:54:01 -0300

    Update  Create

--- handlers/create.go
+++ handlers/create.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
+	"github.com/lib/pq"
 	"github.com/luccascleann/projeto_api/models"
 )
 
@@ -24,9 +27,16 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = models.Insert(user)
+	fmt.Println()
 	if err != nil {
-		log.Printf("Erro ao tentar inserir um usuario ao banco de dados: %v", err)
-		http.Error(w, "500 - Erro ao tentar inserir um usuario ao banco de dados", http.StatusInternalServerError)
+		pgErrorMessage := err.(*pq.Error).Message
+		isPrimaryKeyInDataBase := strings.Contains(pgErrorMessage, "duplicate key value violates unique constraint")
+		if isPrimaryKeyInDataBase {
+			http.Error(w, fmt.Sprintf("400 - O usuario %s ja existe no banco de dados.", user.Username), http.StatusInternalServerError)
+		} else {
+			log.Printf("Erro ao tentar inserir um usuario ao banco de dados: %v", err)
+			http.Error(w, "500 - Erro ao tentar inserir um usuario ao banco de dados", http.StatusInternalServerError)
+		}
 	} else {
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte("201 - usuario criado"))
